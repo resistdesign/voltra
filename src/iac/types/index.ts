997f9692e41a3fc45e9b2ab6734a00e3209dca00
Ryanne Graff <ryantgraff@resistdesign.com>
2024-01-08 22:20:35 -0500

    chore: (repo) Automatic commit.

--- src/iac/types/index.ts
+++ src/iac/types/index.ts
@@ -1,14 +1,17 @@
-import Path from 'path';
-import FS from 'fs';
-import { format } from 'prettier';
-import { NamespaceStructure } from './Types';
-import { CloudFormationResourceSpecificationData } from './CloudFormationResourceSpecification';
-import { getNamespaceStructure } from './Utils';
-import { renderNamespaceStructure } from './Renderers';
+import Path from "path";
+import FS from "fs";
+import { format } from "prettier";
+import { NamespaceStructure } from "./Types";
+import { CloudFormationResourceSpecificationData } from "./CloudFormationResourceSpecification";
+import { getNamespaceStructure } from "./Utils";
+import { renderNamespaceStructure } from "./Renderers";
 
-const STANDARD_INCLUDES = FS.readFileSync(Path.join(__dirname, 'StandardIncludes.d.ts'), { encoding: 'utf8' })
+const STANDARD_INCLUDES = FS.readFileSync(
+  Path.join(__dirname, "StandardIncludes.d.ts"),
+  { encoding: "utf8" },
+)
   // IMPORTANT: Remove the first line which is a placeholder for the `AllResourceTypes` type.
-  .replace(/.*/, '')
+  .replace(/.*/, "")
   .substr(1);
 
 const BASE_NAMESPACE_STRUCTURE: NamespaceStructure = {
@@ -19,11 +22,25 @@ const BASE_NAMESPACE_STRUCTURE: NamespaceStructure = {
   namespaces: {},
 };
 
-const NamespaceStructure: NamespaceStructure = getNamespaceStructure(CloudFormationResourceSpecificationData, BASE_NAMESPACE_STRUCTURE);
+const NamespaceStructure: NamespaceStructure = getNamespaceStructure(
+  CloudFormationResourceSpecificationData,
+  BASE_NAMESPACE_STRUCTURE,
+);
 
-const TypesContentString = format(renderNamespaceStructure(NamespaceStructure), {
-  parser: 'typescript',
-});
+const generate = async () => {
+  const TypesContentString = await format(
+    renderNamespaceStructure(NamespaceStructure),
+    {
+      parser: "typescript",
+    },
+  );
 
-FS.mkdirSync(Path.join(__dirname, '..', 'dist'), { recursive: true });
-FS.writeFileSync(Path.join(__dirname, '..', 'dist', 'index.d.ts'), TypesContentString, { encoding: 'utf8' });
+  FS.mkdirSync(Path.join(__dirname, "..", "dist"), { recursive: true });
+  FS.writeFileSync(
+    Path.join(__dirname, "..", "dist", "index.d.ts"),
+    TypesContentString,
+    { encoding: "utf8" },
+  );
+};
+
+generate();
