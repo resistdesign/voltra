9b008002d76e6bbdd2efa0c043a4d8cbb8adc0c7
Ryanne Graff <ryantgraff@resistdesign.com>
2024-02-19 02:26:19 -0500

    doc: (index) Improve doc comments.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 /**
- * Route RPC back-end API requests to the appropriate functions with context about authentication, the request context and more.
+ * Route RPC back-end API requests to the appropriate functions with context about authentication, the request itself and more.
  * */
 export * as API from "./api";
 /**
- * Build front-end applications with React/TypeScript, layout systems, form generation, easy state management and more.
+ * Build front-end applications with React/TypeScript, layout systems, form generation, RPC requests, easy state management and more.
  * */
 export * as App from "./app";
 /**
