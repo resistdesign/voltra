f9d13511df1e93fdbdcfa5d49c1d5733f86d51ee
Ryanne Graff <ryantgraff@resistdesign.com>
2024-02-19 02:05:35 -0500

    feat: (iac) Organize and document the full IaC namespace structure.

--- src/iac/index.ts
+++ src/iac/index.ts
@@ -1,2 +1,9 @@
-export * from './packs';
-export * from './utils';
+/**
+ * Use packs for easily building infrastructure with reusable components.
+ * */
+export * as Packs from "./packs";
+/**
+ * Utilities allow for easy modification and extension of infrastructure templates.
+ * */
+export * as Utils from "./utils";
+export * from "./SimpleCFT";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+/**
+ * Route RPC back-end API requests to the functions with context about authentication, the request context and more.
+ * */
 export * as API from "./api";
+/**
+ * Build front-end applications with React/TypeScript, layout systems, form generation, easy state management and more.
+ * */
 export * as App from "./app";
+/**
+ * Build infrastructure as code with reusable components and utilities.
+ * */
 export * as IaC from "./iac";
