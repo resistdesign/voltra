fc7d30e451724f72056671e3d02df75efb84e881
Ryanne Graff <ryantgraff@resistdesign.com>
2024-02-20 23:12:06 -0500

    refactor: (api) Improve namespace structure.

--- src/api/ORM/index.ts
+++ src/api/ORM/index.ts
@@ -1,6 +1,6 @@
 export * as Drivers from "./drivers";
 export * as CriteriaUtils from "./CriteriaUtils";
 export * as DataContext from "./DataContextService";
-export * as ServiceType from "./ServiceTypes";
-export * as Search from "./SearchCriteriaTypes";
-export * as TypeUtils from "./TypeStructureUtils";
+export * as ServiceTypes from "./ServiceTypes";
+export * as SearchTypes from "./SearchCriteriaTypes";
+export * as TypeStructure from "./TypeStructureUtils";
