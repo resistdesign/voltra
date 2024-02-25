f788208cb54b927fdbc90764d561f4ea91de71f1
Ryanne Graff <ryantgraff@resistdesign.com>
2024-02-25 09:51:28 -0500

    chore: (repo) Automatic commit.

--- src/api/ORM/TypeStructureUtils.ts
+++ src/api/ORM/TypeStructureUtils.ts
@@ -51,76 +51,11 @@ export const typeStructureToDataContext = (
     comboType = false,
   } = getCleanTypeStructure(typeStructure, typeStructureMap);
   const cleanFullTypeName = getCleanType(type, name);
-  const uniquelyIdentifyingFieldName = getCleanedTagStringValue(
-    getTagValue("uniquelyIdentifyingFieldName", typeStructure),
-  );
-  const allowedOperations = getCleanedTagStringValue(
-    getTagValue("allowedOperations", typeStructure),
-  );
-  const uuidFieldName =
-    typeof uniquelyIdentifyingFieldName === "string"
-      ? uniquelyIdentifyingFieldName.trim()
-      : undefined;
-  const opsList = allowedOperations
-    ? allowedOperations.split(",").map((o) => o.trim())
-    : undefined;
+  const cachedDataContext = cache[cleanFullTypeName];
 
-  let newDataContext = !!cache[cleanFullTypeName]
-    ? cache[cleanFullTypeName]
-    : {
-        itemTypeName: cleanFullTypeName,
-        resolvedType: type,
-        isTypeAlias: cleanFullTypeName in typeStructureMap,
-        uniquelyIdentifyingFieldName: uuidFieldName,
-        allowedOperations: opsList as DataContextOperationOptions[],
-        fields: {},
-      };
-
-  if (comboType) {
-    // `content` is a list of types.
-    for (const subType of content) {
-      const {
-        allowedOperations: subTypeAllowedOperations = [],
-        fields: subTypeFields = {},
-      } = typeStructureToDataContext(subType, typeStructureMap, cache);
-      const {
-        allowedOperations: newDataContextAllowedOperations = [],
-        fields: newDataContextFields = {},
-      } = newDataContext;
-
-      // Merge data contexts.
-      newDataContext = {
-        ...newDataContext,
-        allowedOperations: [
-          ...newDataContextAllowedOperations,
-          ...subTypeAllowedOperations,
-        ],
-        fields: {
-          ...newDataContextFields,
-          ...subTypeFields,
-        },
-      };
-    }
+  if (cachedDataContext) {
+    return cachedDataContext;
   } else {
-    // TODO: `content` is a list of fields.
-  }
-
-  return newDataContext;
-};
-
-/**
- * Create a {@link DataContextMap} from a {@link TypeStructureMap} generated from TypeScript types.
- * */
-export const typeStructureMapToDataContextMap = (
-  typeStructureMap: TypeStructureMap,
-  cache: DataContextMap = {},
-): DataContextMap => {
-  // TODO: Detect caching by map key???
-  const dataContextMap: DataContextMap = cache;
-
-  Object.entries(typeStructureMap).forEach(([key, typeStructure]) => {
-    const { namespace, type, content = [], comboType = false } = typeStructure;
-    const cleanFullTypeName = getCleanType(type, namespace);
     const uniquelyIdentifyingFieldName = getCleanedTagStringValue(
       getTagValue("uniquelyIdentifyingFieldName", typeStructure),
     );
@@ -134,32 +69,79 @@ export const typeStructureMapToDataContextMap = (
     const opsList = allowedOperations
       ? allowedOperations.split(",").map((o) => o.trim())
       : undefined;
-    const newDataContext = !!cache[cleanFullTypeName]
+
+    let newDataContext = !!cache[cleanFullTypeName]
       ? cache[cleanFullTypeName]
       : {
-          itemTypeName: key,
+          itemTypeName: cleanFullTypeName,
           resolvedType: type,
-          isTypeAlias: !(cleanFullTypeName === key),
+          isTypeAlias: cleanFullTypeName in typeStructureMap,
           uniquelyIdentifyingFieldName: uuidFieldName,
           allowedOperations: opsList as DataContextOperationOptions[],
-          // TODO: What about combo types? (need to merge in combo type fields)
-          fields: content.reduce((acc, subType) => {
-            const { name, comboType = false } = subType;
-
-            if (comboType) {
-              // TODO: Merge in fields.
-            } else {
-              // Just apply the current field.
-              return {
-                ...acc,
-                [name]: typeStructureToDataContextField(subType),
-              };
-            }
-          }, {}),
+          fields: {},
         };
 
-    dataContextMap[key] = newDataContext;
+    if (comboType) {
+      // `content` is a list of types.
+      for (const subType of content) {
+        const {
+          allowedOperations: subTypeAllowedOperations = [],
+          fields: subTypeFields = {},
+        } = typeStructureToDataContext(subType, typeStructureMap, cache);
+        const {
+          allowedOperations: newDataContextAllowedOperations = [],
+          fields: newDataContextFields = {},
+        } = newDataContext;
+
+        // Merge data contexts.
+        newDataContext = {
+          ...newDataContext,
+          allowedOperations: [
+            ...newDataContextAllowedOperations,
+            ...subTypeAllowedOperations,
+          ],
+          fields: {
+            ...newDataContextFields,
+            ...subTypeFields,
+          },
+        };
+      }
+    } else {
+      // `content` is a list of fields.
+      for (const field of content) {
+        const { name: fieldName } = field;
+        const fieldDataContext = typeStructureToDataContextField(field);
+
+        newDataContext.fields[fieldName] = fieldDataContext;
+      }
+    }
+
     cache[cleanFullTypeName] = newDataContext;
+
+    return newDataContext;
+  }
+};
+
+/**
+ * Create a {@link DataContextMap} from a {@link TypeStructureMap} generated from TypeScript types.
+ * */
+export const typeStructureMapToDataContextMap = (
+  typeStructureMap: TypeStructureMap,
+): DataContextMap => {
+  const dataContextMap: DataContextMap = {};
+
+  Object.entries(typeStructureMap).forEach(([_key, typeStructure]) => {
+    const { namespace, type } = typeStructure;
+    const cleanFullTypeName = getCleanType(type, namespace);
+    const existingDataContext = dataContextMap[cleanFullTypeName];
+
+    dataContextMap[cleanFullTypeName] = !!existingDataContext
+      ? existingDataContext
+      : typeStructureToDataContext(
+          typeStructure,
+          typeStructureMap,
+          dataContextMap,
+        );
   });
 
   return dataContextMap;
