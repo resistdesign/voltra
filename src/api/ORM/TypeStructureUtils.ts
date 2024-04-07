01f75e9a3abf97b72b8628789648eaf06966d668
Ryanne Graff <ryantgraff@resistdesign.com>
2024-04-06 22:10:27 -0400

    chore: (repo) Automatic commit.

--- src/api/ORM/TypeStructureUtils.ts
+++ src/api/ORM/TypeStructureUtils.ts
@@ -201,7 +201,7 @@ export const typeStructureMapToDataContextMap = (
     );
     const cleanFullTypeName = getCleanType(type, namespace);
     // TODO: Losing pick/omit info here, because `type` is being used as the key.
-    // TODO: May need to keep the pick/omit types but mark them as not real databases.
+    // TODO: May need to keep the pick/omit types but mark them as not real database tables.
     const existingDataContext = dataContextMap[cleanFullTypeName];
 
     if (!isVirtual) {
