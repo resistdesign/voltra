bd3f8dbe8be92ede9d62e7ca57523cb62a9cb9aa
Ryanne Graff <ryantgraff@resistdesign.com>
2024-06-26 21:50:44 -0400

    chore: (repo) Automatic commit.

--- src/api/ORM/index.ts
+++ src/api/ORM/index.ts
@@ -3,4 +3,4 @@ export * as CriteriaUtils from "./CriteriaUtils";
 export * as DataContext from "./DataContextService";
 export * as ServiceTypes from "./ServiceTypes";
 export * as SearchTypes from "./SearchCriteriaTypes";
-export * as TypeStructure from "./TypeStructureUtils";
+// TODO: Fully automated ORM.
