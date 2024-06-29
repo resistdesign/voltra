84e87ba5db0cf2719cb0b33bcfe1f03a62d23710
Ryanne Graff <ryantgraff@resistdesign.com>
2024-06-29 09:15:41 -0400

    chore: (repo) Automatic commit.

--- src/app/components/FormGen/Types.ts
+++ src/app/components/FormGen/Types.ts
@@ -1,10 +1,14 @@
 import { FieldInfo } from "aws-sdk/clients/configservice";
-import { TypeInfoMap } from "../../../common/TypeParsing/TypeInfo";
+import { TypeInfo, TypeInfoMap } from "../../../common/TypeParsing/TypeInfo";
 
 export type InputProps<ElementType> = ElementType & {
   typeInfoMap: TypeInfoMap;
+  typeInfo: TypeInfo;
   fieldInfo: FieldInfo;
+  index?: number;
+  options?: any[];
   value: any;
   onChange: (value: any) => void;
   onNavigate: (path: string) => void;
+  onOptionsSearch: (query: any) => void;
 };
