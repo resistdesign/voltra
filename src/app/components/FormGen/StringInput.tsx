23702caf61c1527db0360321dd307f135ce3995b
Ryanne Graff <ryantgraff@resistdesign.com>
2024-06-29 09:52:38 -0400

    chore: (repo) Automatic commit.

--- src/app/components/FormGen/StringInput.tsx
+++ src/app/components/FormGen/StringInput.tsx
@@ -1,9 +1,26 @@
-import {InputHTMLAttributes} from "react";
+import {
+  ChangeEvent as ReactChangeEvent,
+  FC,
+  InputHTMLAttributes,
+  useCallback,
+} from "react";
+import { InputProps } from "./Types";
 
-export type StringInputProps = InputHTMLAttributes<HTMLInputElement>;
+export type StringInputProps = InputProps<
+  InputHTMLAttributes<HTMLInputElement>
+>;
 
-export const StringInput: FC = () => {
-  return (
-    <input type="text" />
+export const StringInput: FC<StringInputProps> = ({
+  value,
+  onChange,
+  ...rest
+}) => {
+  const onChangeHandler = useCallback(
+    ({ target: { value: newValue } }: ReactChangeEvent<HTMLInputElement>) => {
+      onChange(newValue);
+    },
+    [onChange],
   );
+
+  return <input type="text" value={`${value ?? ""}`} {...rest} />;
 };
