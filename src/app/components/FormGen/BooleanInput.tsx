ceae7b52463d070e74f6caa84c73e9fb59d93a4b
Ryanne Graff <ryantgraff@resistdesign.com>
2024-06-29 14:11:03 -0400

    chore: (repo) Automatic commit.

--- src/app/components/FormGen/BooleanInput.tsx
+++ src/app/components/FormGen/BooleanInput.tsx
@@ -1,25 +1,18 @@
-import {
-  ChangeEvent as ReactChangeEvent,
-  InputHTMLAttributes,
-  useCallback,
-} from "react";
+import { InputHTMLAttributes, useCallback } from "react";
 import { InputComponent } from "./Types";
 
 export const BooleanInput: InputComponent<
   InputHTMLAttributes<HTMLInputElement>
 > = ({ value, onChange, ...rest }) => {
-  const onChangeHandler = useCallback(
-    ({ target: { value: newValue } }: ReactChangeEvent<HTMLInputElement>) => {
-      onChange(newValue ?? false);
-    },
-    [onChange],
-  );
+  const onClickHandler = useCallback(() => {
+    onChange(!value);
+  }, [value, onChange]);
 
   return (
     <input
       type="checkbox"
       checked={value as boolean}
-      onChange={onChangeHandler}
+      onClick={onClickHandler}
       {...rest}
     />
   );
