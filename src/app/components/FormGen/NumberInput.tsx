ec9f6c861280396dc9393e8467b0a9fda79aba9d
Ryanne Graff <ryantgraff@resistdesign.com>
2024-06-29 10:36:17 -0400

    chore: (repo) Automatic commit.

--- src/app/components/FormGen/NumberInput.tsx
+++ src/app/components/FormGen/NumberInput.tsx
@@ -7,27 +7,37 @@ import {
 } from "react";
 import { InputComponentProps } from "./Types";
 
+const isExponent = (value: string) =>
+  value.includes("e") || value.includes("E");
+
 export const NumberInput: InputComponentProps<
   InputHTMLAttributes<HTMLInputElement>
 > = ({ value, onChange, ...rest }) => {
-  const [internalValue, setInternalValue] = useState(value ?? "");
+  const [internalNumericValue, setInternalNumericValue] = useState(value ?? 0);
+  const [internalValue, setInternalValue] = useState(value ?? "0");
   const onChangeHandler = useCallback(
     ({ target: { value: newValue } }: ReactChangeEvent<HTMLInputElement>) => {
       try {
-        const newNumberValue = JSON.parse(`${newValue ?? ""}`);
+        const newNumberValue = JSON.parse(`${newValue ?? "0"}`);
 
-        setInternalValue(newNumberValue ?? "");
-        onChange(newNumberValue);
+        setInternalNumericValue(newNumberValue);
       } catch (error) {
-        setInternalValue(newValue ?? "");
+        // Ignore.
       }
+
+      setInternalValue(newValue ?? "0");
     },
     [onChange],
   );
 
   useEffect(() => {
     setInternalValue(value ?? "");
+    setInternalNumericValue(value ?? 0);
   }, [value]);
+
+  useEffect(() => {
+    onChange(internalValue);
+  }, [internalNumericValue, onChange]);
 
   return (
     <input
