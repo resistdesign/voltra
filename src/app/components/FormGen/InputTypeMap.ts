e4bbf454a07d5a7da72951724aea7fad0fcd8da4
Ryanne Graff <ryantgraff@resistdesign.com>
2024-06-29 23:29:06 -0400

    chore: (repo) Automatic commit.

--- src/app/components/FormGen/InputTypeMap.ts
+++ src/app/components/FormGen/InputTypeMap.ts
@@ -7,15 +7,20 @@ import { BooleanInput } from "./Inputs/Primitives/BooleanInput";
 //  [x] string
 //  [x] number
 //  [x] boolean
+//  Primitive options selection:
 //  [] option selector
 //  [] option selector w/ custom value
 //  [] option selector w/search
 //  [] option selector w/search w/ custom value
+//  ---
 //  [] custom (i.e. date picker)
+//  Designate primary field for object selection:
 //  [] existing object selector
 //  [] existing object selector w/ search
 //  [] existing object selector w/ advanced search
+//  ---
 //  [] object forms and sub-forms
+//  ---
 //  [] array of all of the above
 
 export const INPUT_TYPE_MAP: Record<string, InputComponent<any>> = {
