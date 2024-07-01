4cc47f2ffe568c51f80ea35586557d2acc1a33f7
Ryanne Graff <ryantgraff@resistdesign.com>
2024-07-01 12:32:14 -0400

    chore: (repo) Automatic commit.

--- src/app/components/FormGen/Types.ts
+++ src/app/components/FormGen/Types.ts
@@ -3,7 +3,6 @@ import { FC, InputHTMLAttributes } from "react";
 export type InputProps<ElementPropsType> = ElementPropsType & {
   value: any;
   onChange: (value: any) => void;
-  // TODO: Required fields.
   options?: any[];
 };
 
