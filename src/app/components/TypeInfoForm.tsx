6a9e3fc93401efbba44ba19e3944557cced7857e
Ryanne Graff <ryantgraff@resistdesign.com>
2024-07-03 18:52:50 -0400

    chore: (repo) Automatic commit.

--- src/app/components/TypeInfoForm.tsx
+++ src/app/components/TypeInfoForm.tsx
@@ -1,10 +1,74 @@
 import { InputComponent } from "./TypeInfoForm/Types";
 import { Form } from "./Form";
+import { FC, useCallback, useEffect, useMemo, useState } from "react";
+import {
+  TypeInfo,
+  TypeInfoField,
+  TypeInfoMap,
+} from "../../common/TypeParsing/TypeInfo";
+import { getInputType } from "./TypeInfoForm/InputTypeMapUtils";
 
-export const TypeInfoForm: InputComponent<HTMLFormElement> = (
-  {
-    // TODO: Type name and input props.
-  },
-) => {
-  return <Form></Form>;
+export type TypeInfoFormProps<TypeInfoMapType extends TypeInfoMap> = {
+  typeInfoMap: TypeInfoMapType;
+  typeInfoName: keyof TypeInfoMapType;
+  customInputTypeMap?: Record<string, InputComponent<any>>;
+  value: Record<any, any>;
+  onChange: (value: Record<any, any>) => void;
+};
+
+export const TypeInfoForm: FC<TypeInfoFormProps<any>> = ({
+  typeInfoMap,
+  typeInfoName,
+  customInputTypeMap = {},
+  value,
+  onChange,
+}) => {
+  const typeInfo = useMemo<TypeInfo | undefined>(() => {
+    return typeInfoMap[typeInfoName];
+  }, [typeInfoMap, typeInfoName]);
+  const fields = useMemo<Record<string, TypeInfoField>>(() => {
+    const { fields: typeInfoFields = {} }: Partial<TypeInfo> = typeInfo || {};
+
+    return typeInfoFields;
+  }, [typeInfo]);
+  const [internalValue, setInternalValue] = useState<Record<any, any>>(value);
+  const onSubmit = useCallback(() => {
+    onChange(internalValue);
+  }, [internalValue, onChange]);
+
+  useEffect(() => {
+    setInternalValue(value);
+  }, [value]);
+
+  return (
+    <Form onSubmit={onSubmit}>
+      {Object.keys(fields).map((fieldName) => {
+        const field = fields[fieldName];
+        const {
+          type: fieldType,
+          // TODO: Get from tags, BUT expose in API somehow.
+          // customInputType,
+        } = field;
+        const InputComponent = getInputType(fieldType, customInputTypeMap);
+        const fieldValue = internalValue[fieldName];
+        const onFieldChange = useCallback(
+          (newValue: any) => {
+            setInternalValue({
+              ...internalValue,
+              [fieldName]: newValue,
+            });
+          },
+          [internalValue, fieldName],
+        );
+
+        return InputComponent ? (
+          <InputComponent
+            key={fieldName}
+            value={fieldValue}
+            onChange={onFieldChange}
+          />
+        ) : undefined;
+      })}
+    </Form>
+  );
 };
