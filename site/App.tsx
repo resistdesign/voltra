886f3398cd2415b00a0bf75c77342d7de520318f
Ryanne Graff <ryantgraff@resistdesign.com>
2024-07-03 18:57:27 -0400

    chore: (repo) Automatic commit.

--- site/App.tsx
+++ site/App.tsx
@@ -1,17 +1,9 @@
-import {
-  ChangeEvent as ReactChangeEvent,
-  FC,
-  useCallback,
-  useState,
-} from "react";
+import { FC, useState } from "react";
 import { createGlobalStyle } from "styled-components";
 import { ApplicationStateProvider, Route } from "../src/app/utils";
-import { NumberInput } from "../src/app/components/TypeInfoForm/Inputs/Primitives/NumberInput";
-import { BooleanInput } from "../src/app/components/TypeInfoForm/Inputs/Primitives/BooleanInput";
-import { StringSelector } from "../src/app/components/TypeInfoForm/Inputs/PrimitiveOptionSelectors/StringSelector";
-import { NumberSelector } from "../src/app/components/TypeInfoForm/Inputs/PrimitiveOptionSelectors/NumberSelector";
-import { StringComboBox } from "../src/app/components/TypeInfoForm/Inputs/PrimitiveOptionSelectors/StringComboBox";
-import { NumberComboBox } from "../src/app/components/TypeInfoForm/Inputs/PrimitiveOptionSelectors/NumberComboBox";
+import { TypeInfoForm } from "../src/app/components/TypeInfoForm";
+import { TypeInfoMap } from "../src/common/TypeParsing/TypeInfo";
+import { getTypeInfoMapFromTypeScript } from "../src/common/TypeParsing";
 
 const GlobalStyle = createGlobalStyle`
     html,
@@ -28,33 +20,20 @@ const GlobalStyle = createGlobalStyle`
     }
 `;
 
-export const App: FC = () => {
-  const [val, setVal] = useState<number>(1e86);
-  const onNumberInputChange = useCallback((value: any) => {
-    setVal(value as number);
-  }, []);
-  const onBasicInputChange = useCallback(
-    ({
-      target: { value: newValue = "0" },
-    }: ReactChangeEvent<HTMLInputElement>) => {
-      setVal(Number(newValue));
-    },
-    [],
-  );
-  const [boolVal, setBoolVal] = useState<boolean>(false);
-  const onBooleanInputChange = useCallback((newValue: any) => {
-    setBoolVal(newValue as boolean);
-  }, []);
-  const [selectedString, setSelectedString] = useState<string | undefined>();
-  const onStringSelectorChange = useCallback((newValue: any) => {
-    setSelectedString(newValue as string);
-  }, []);
-  const [selectedNumber, setSelectedNumber] = useState<number | undefined>();
-  const onNumberSelectorChange = useCallback((newValue: any) => {
-    setSelectedNumber(newValue as number);
-  }, []);
+const DEMO_TS = `
+export type Person = {
+  firstName: string;
+  lastName: string;
+  age: number;
+  phoneNumber: string;
+  email: string;
+  friends: Person[];
+};
+`;
+const DEMO_TYPE_INFO_MAP: TypeInfoMap = getTypeInfoMapFromTypeScript(DEMO_TS);
 
-  console.log(val, typeof val, boolVal, selectedString, selectedNumber);
+export const App: FC = () => {
+  const [value, setValue] = useState<Record<any, any>>({});
 
   return (
     <ApplicationStateProvider>
@@ -63,36 +42,11 @@ export const App: FC = () => {
         <h1>Demos</h1>
         <a href="https://docs.voltra.app/docs">Docs</a>
         <br />
-        <NumberInput value={val} onChange={onNumberInputChange} type="number" />
-        <br />
-        <input type="number" value={val} onChange={onBasicInputChange} />
-        <br />
-        <BooleanInput value={boolVal as any} onChange={onBooleanInputChange} />
-        <br />
-        <input value={selectedString ?? ""} readOnly />
-        <StringSelector
-          value={selectedString}
-          onChange={onStringSelectorChange}
-          options={["one", "two", "three"]}
-        />
-        <br />
-        <input value={selectedNumber ?? ""} readOnly />
-        <NumberSelector
-          value={selectedNumber}
-          onChange={onNumberSelectorChange}
-          options={[1, 2, 3]}
-        />
-        <br />
-        <StringComboBox
-          value={selectedString}
-          onChange={onStringSelectorChange}
-          options={[undefined, "one", "two", "three"]}
-        />
-        <br />
-        <NumberComboBox
-          value={selectedNumber}
-          onChange={onNumberSelectorChange}
-          options={[undefined, 1, 2, 3]}
+        <TypeInfoForm
+          typeInfoMap={DEMO_TYPE_INFO_MAP}
+          typeInfoName={"Person"}
+          value={value}
+          onChange={setValue}
         />
       </Route>
     </ApplicationStateProvider>
