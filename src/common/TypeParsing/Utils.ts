61231fdba0ef6dd6db49baff144667b3fc999ec9
Ryanne Graff <ryantgraff@resistdesign.com>
2024-07-11 21:11:33 -0400

    chore: (repo) Automatic commit.

--- src/common/TypeParsing/Utils.ts
+++
@@ -1,6 +0,0 @@
-import { BUILTIN_TYPE_NAMES } from "./Constants";
-
-export const typeNameIsBuiltIn = (typeName: string): boolean =>
-  BUILTIN_TYPE_NAMES.includes(typeName) ||
-  (typeName in globalThis &&
-    typeof (globalThis as any)[typeName] === "function");
