990ccbf396256955849f165d0875f88056cc80fe
Ryanne Graff <ryantgraff@resistdesign.com>
2024-07-13 14:22:44 -0400

    chore: (repo) Automatic commit.

--- src/app/components/TypeInfoComponents/TypeInfoApplication.tsx
+++ src/app/components/TypeInfoComponents/TypeInfoApplication.tsx
@@ -1,7 +1,7 @@
-import { FC } from "react";
+import { FC, useMemo, useState } from "react";
 import { TypeInfoForm } from "./TypeInfoForm";
 import { TypeInfoMap } from "../../../common/TypeParsing/TypeInfo";
-import { InputComponent, TypeInfoDataStructure } from "./Types";
+import { InputComponent, TypeInfoDataStructure, TypeNavigation } from "./Types";
 
 export type TypeInfoApplicationProps = {
   typeInfoMap: TypeInfoMap;
@@ -18,5 +18,14 @@ export const TypeInfoApplication: FC<TypeInfoApplicationProps> = ({
   value,
   onChange,
 }) => {
+  const [typeNavigationHistory, setTypeNavigationHistory] =
+    useState<TypeNavigation[]>();
+  const currentTypeNavigation = useMemo(() => {
+    return typeNavigationHistory?.[typeNavigationHistory.length - 1];
+  }, [typeNavigationHistory]);
+  // TODO: How to navigate to the correct object (by primary key) when navigating to a type.
+  // TODO: How to return from type navigation and apply the new value to the related field on the correct object.
+  // TODO: What is the UX around Done VS Submit????
+
   return <TypeInfoForm />;
 };
