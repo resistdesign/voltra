7972304c9c447e49996afe114286dc60c406a2a3
Ryanne Graff <ryantgraff@resistdesign.com>
2024-07-17 19:30:46 -0400

    chore: (repo) Automatic commit.

---
+++ src/common/SearchTypes.ts
@@ -0,0 +1,32 @@
+export enum FieldOperators {
+  EQUALS = "EQUALS",
+  NOT_EQUALS = "NOT_EQUALS",
+  GREATER_THAN = "GREATER_THAN",
+  GREATER_THAN_OR_EQUAL = "GREATER_THAN_OR_EQUAL",
+  LESS_THAN = "LESS_THAN",
+  LESS_THAN_OR_EQUAL = "LESS_THAN_OR_EQUAL",
+  IN = "IN",
+  NOT_IN = "NOT_IN",
+  LIKE = "LIKE",
+  NOT_LIKE = "NOT_LIKE",
+  IS_NULL = "IS_NULL",
+  IS_NOT_NULL = "IS_NOT_NULL",
+  BETWEEN = "BETWEEN",
+  NOT_BETWEEN = "NOT_BETWEEN",
+  CONTAINS = "CONTAINS",
+  NOT_CONTAINS = "NOT_CONTAINS",
+}
+
+export type FieldCriterion = {
+  fieldName: string;
+  operator?: FieldOperators;
+  customOperator?: string;
+  value?: any;
+  subSearchCriteria?: SearchCriteria;
+};
+
+export type SearchCriteria = {
+  pageData?: any;
+  itemType: string;
+  fieldCriteria: FieldCriterion[];
+};
