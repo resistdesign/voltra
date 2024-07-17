4bf93ecaca29ee8ae6d7c00fc7a7f9a6e7ec9139
Ryanne Graff <ryantgraff@resistdesign.com>
2024-07-17 12:14:25 -0400

    chore: (repo) Automatic commit.

---
+++ src/app/components/TypeInfoComponents/Inputs/ObjectSearch.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+// TODO: Search Criteria structures.
+// TODO: Search criteria interface.
+
+export type ObjectSearchProps = {};
+
+export const ObjectSearch: FC<ObjectSearchProps> = () => {
+  return <div></div>;
+};
