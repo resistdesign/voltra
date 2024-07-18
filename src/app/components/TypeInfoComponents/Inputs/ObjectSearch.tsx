dec81a10b5639bc3e7bd03e693793eed46c8d483
Ryanne Graff <ryantgraff@resistdesign.com>
2024-07-18 13:04:49 -0400

    chore: (repo) Automatic commit.

--- src/app/components/TypeInfoComponents/Inputs/ObjectSearch.tsx
+++ src/app/components/TypeInfoComponents/Inputs/ObjectSearch.tsx
@@ -1,12 +1,21 @@
 import { FC } from "react";
 import { SearchCriteria } from "../../../../common/SearchTypes";
+import { TypeInfoDataItem } from "../Types";
+import { TypeInfo } from "../../../../common/TypeParsing/TypeInfo";
+import { ObjectTable } from "./ObjectTable";
 
 // TODO: Search criteria interface.
 
 export type ObjectSearchProps = {
+  typeInfo: TypeInfo;
   searchCriteria: SearchCriteria;
+  searchResults: TypeInfoDataItem[];
 };
 
-export const ObjectSearch: FC<ObjectSearchProps> = ({ searchCriteria }) => {
-  return <div></div>;
+export const ObjectSearch: FC<ObjectSearchProps> = ({
+  typeInfo,
+  searchCriteria,
+  searchResults = [],
+}) => {
+  return <ObjectTable typeInfo={typeInfo} objectList={searchResults} />;
 };
