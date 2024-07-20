babce34379d8f0cd51a4c5f9825384e771e863cb
Ryanne Graff <ryantgraff@resistdesign.com>
2024-07-19 20:42:49 -0400

    chore: (repo) Automatic commit.

--- src/api/ORM/CriteriaUtils/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./CriteriaUtils";
-export * from "./CriterionDriverResolutionTools";
--- src/api/ORM/index.ts
+++ src/api/ORM/index.ts
@@ -1,6 +1,5 @@
 export * as Drivers from "./drivers";
-export * as CriteriaUtils from "./CriteriaUtils";
 export * as DataContext from "./DataContextService";
 export * as ServiceTypes from "./ServiceTypes";
 export * as SearchTypes from "./SearchCriteriaTypes";
-// TODO: Fully automated ORM.
+// TODO: Fully automated ORM. (It's on the way with TypeInfo!!!)
