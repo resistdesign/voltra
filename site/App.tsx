6dfaa637ba9b2a75b00f457cbd4b2e39ad3f92ab
Ryanne Graff <ryantgraff@resistdesign.com>
2024-07-24 22:57:39 -0400

    chore: (repo) Automatic commit.

--- site/App.tsx
+++ site/App.tsx
@@ -1,12 +1,7 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import { createGlobalStyle } from "styled-components";
 import { ApplicationStateProvider, Route } from "../src/app/utils";
-import { TypeInfoMap } from "../src/common/TypeParsing/TypeInfo";
-import { getTypeInfoMapFromTypeScript } from "../src/common/TypeParsing";
-import {
-  TypeInfoApplication,
-  TypeInfoDataStructure,
-} from "../src/app/components";
+import { TypeInfo } from "./App/TypeInfo";
 
 const GlobalStyle = createGlobalStyle`
     html,
@@ -28,98 +23,20 @@ const GlobalStyle = createGlobalStyle`
     }
 `;
 
-const DEMO_TS = `
-export type PersistableItem = {
-  /**
-  * @label ID
-  * @primaryField
-  * @hidden
-  */
-  readonly id?: string;
-};
-
-export type Car = PersistableItem & {
-  /**
-  * @label Make
-  */
-  make: string;
-  /**
-  * @label Model
-  */
-  model: string;
-  /**
-  * @label Year
-  * @constraints.min 1900
-  * @constraints.max 2022
-  * @constraints.step 1
-  */
-  year: number;
-};
-
-export type Person = PersistableItem & {
-  /**
-  * @label First Name
-  */
-  firstName: string;
-  /**
-  * @label Last Name
-  */
-  lastName: string;
-  /**
-  * @label Age
-  * @constraints.defaultValue "18.0"
-  * @constraints.step 0.5
-  * @constraints.min 18.0
-  * @constraints.max 150.0
-  * @constraints.pattern \\d*\\.\\d+
-  */
-  age: number;
-  /**
-  * @label Phone Number (+### (###) ###-####)
-  * @format tel
-  * @ Note #constraints.pattern ^\\+\\d+(-\\d+)? \\(\\d{3}\\) \\d{3}-\\d{4}$
-  */
-  phoneNumber: string;
-  /**
-  * @label Email
-  * @format email
-  */
-  email: string;
-  /**
-  * @label Car
-  */
-  car: Car;
-  /**
-  * @label Likes Cheese
-  * @constraints.defaultValue
-  */
-  readonly likesCheese: boolean;
-  /**
-  * @label Dietary Restrictions
-  */
-  dietaryRestrictions: "Vegan" | "Vegitarian" | "Pescatarian" | "Keto" | "Paleo" | "None";
-};
-`;
-const DEMO_TYPE_INFO_MAP: TypeInfoMap = getTypeInfoMapFromTypeScript(DEMO_TS);
-
 export const App: FC = () => {
-  const [value, setValue] = useState<TypeInfoDataStructure>({});
-
-  console.log("Structure:", value);
-
   return (
     <ApplicationStateProvider>
       <GlobalStyle />
       <Route>
-        <h1>Demos</h1>
+        <h1>Voltra</h1>
+        <h3>Info</h3>
         <a href="https://docs.voltra.app/docs">Docs</a>
+        <h3>Demos</h3>
+        <a href="type-info">Type Info</a>
         <br />
-        <TypeInfoApplication
-          typeInfoMap={DEMO_TYPE_INFO_MAP}
-          typeInfoName="Person"
-          value={value}
-          onChange={setValue}
-        />
+        <Route path="type-info">
+          <TypeInfo />
+        </Route>
       </Route>
     </ApplicationStateProvider>
   );
