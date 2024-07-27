85f412075eb4aeb91adaa30276dc7ea9a16be33c
Ryanne Graff <ryantgraff@resistdesign.com>
2024-07-27 00:24:56 -0400

    chore: (repo) Automatic commit.

--- src/api/ORM/TypeInfoORMService.ts
+++ src/api/ORM/TypeInfoORMService.ts
@@ -10,12 +10,8 @@ import {
 } from "../../common/TypeParsing/Validation";
 import { TypeInfoDataItem } from "../../app/components";
 
-export type DBServiceItemDriverMap = Record<
-  string,
-  DBServiceItemDriver<any, any>
->;
-
 export const TypeInfoORMServiceErrors = {
+  NO_DRIVERS_SUPPLIED: "NO_DRIVERS_SUPPLIED",
   INVALID_TYPE_INFO: "INVALID_TYPE_INFO",
   INVALID_DRIVER: "INVALID_DRIVER",
 };
@@ -23,9 +19,14 @@ export const TypeInfoORMServiceErrors = {
 export class TypeInfoORMService {
   constructor(
     protected typeInfoMap: TypeInfoMap,
-    protected driverMap: DBServiceItemDriverMap,
+    protected driver?: DBServiceItemDriver<any, any>,
+    protected getDriver?: (typeName: string) => DBServiceItemDriver<any, any>,
     protected customValidators?: CustomTypeInfoFieldValidatorMap,
-  ) {}
+  ) {
+    if (!driver && !getDriver) {
+      throw new Error(TypeInfoORMServiceErrors.NO_DRIVERS_SUPPLIED);
+    }
+  }
 
   protected getTypeInfo = (typeName: string): TypeInfo => {
     const typeInfo = this.typeInfoMap[typeName];
@@ -37,8 +38,14 @@ export class TypeInfoORMService {
     return typeInfo;
   };
 
-  protected getDriver = (typeName: string): DBServiceItemDriver<any, any> => {
-    const driver = this.driverMap[typeName];
+  protected getDriverInternal = (
+    typeName: string,
+  ): DBServiceItemDriver<any, any> => {
+    const driver = this.driver
+      ? this.driver
+      : this.getDriver
+        ? this.getDriver(typeName)
+        : undefined;
 
     if (!driver) {
       throw new Error(TypeInfoORMServiceErrors.INVALID_DRIVER);
@@ -69,12 +76,11 @@ export class TypeInfoORMService {
   create = async (typeName: string, item: TypeInfoDataItem): Promise<any> => {
     this.validate(typeName, item, TypeOperation.create);
 
-    const driver = this.getDriver(typeName);
+    const driver = this.getDriverInternal(typeName);
     const newItem = await driver.createItem(item);
 
     // TODO: NESTING: No nesting, just id references
     //  OR maybe nothing at all and use a separate API for relationships.
-    // TODO: Should there even be a driver MAP???
 
     return newItem;
   };
