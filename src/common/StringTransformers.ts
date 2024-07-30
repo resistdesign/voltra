b182d5e6ce55d0a3455d5e5081ee05003588bbda
Ryanne Graff <ryantgraff@resistdesign.com>
2024-07-29 23:13:15 -0400

    chore: (repo) Automatic commit.

--- src/common/StringTransformers.ts
+++ src/common/StringTransformers.ts
@@ -1,16 +1,28 @@
 import { TypeKeyword } from "./TypeParsing/TypeInfo";
 
+/**
+ * A function that transforms a value into a string.
+ * */
 export type StringTransformer = (value: any) => string;
 
+/**
+ * Transform a value into a string when a string is expected.
+ * */
 const transformWhenStringExpected = (value: any) =>
   (value ?? false) === false ? "" : `${value}`;
 
+/**
+ * A map of string transformers for each type keyword.
+ * */
 export const STRING_TRANSFORMERS: Record<TypeKeyword, StringTransformer> = {
   string: transformWhenStringExpected,
   number: transformWhenStringExpected,
   boolean: (value: any) => (value ? "true" : "false"),
 };
 
+/**
+ * A map of custom string transformers for each custom type.
+ * */
 export const CUSTOM_STRING_TRANSFORMERS: Record<string, StringTransformer> = {
   date: (value: any) => (value as Date).toISOString(),
   time: (value: any) => (value as Date).toISOString(),
@@ -20,6 +32,9 @@ export const CUSTOM_STRING_TRANSFORMERS: Record<string, StringTransformer> = {
   unknown: (value: any) => value.toString(),
 };
 
+/**
+ * Transform a value into a string based on the type keyword or custom type.
+ * */
 export const transformValueToString = (
   value: any,
   typeKeyword: TypeKeyword,
