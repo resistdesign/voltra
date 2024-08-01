1144af7e3969731ed728bbcfe8ef3eb8ea68be04
Ryanne Graff <ryantgraff@resistdesign.com>
2024-07-31 21:01:52 -0400

    chore: (repo) Automatic commit.

--- src/api/ORM/DataAccessControl.ts
+++ src/api/ORM/DataAccessControl.ts
@@ -1,14 +1,21 @@
+export enum DACResourceType {
+  PREFIX = "PREFIX",
+  SUFFIX = "SUFFIX",
+  EQUALS = "EQUALS",
+  CONTAINS = "CONTAINS",
+}
+
 export type DACConstraint = {
   id: string;
+  allow: boolean;
+  resource: string;
+  type: DACResourceType;
 };
 
-export type DACRole = {
+export type DACContainer = {
   id: string;
+  parentIds: string[];
   constraints: DACConstraint[];
 };
 
-export type DACUser = {
-  id: string;
-  roles: DACRole[];
-  constraints: DACConstraint[];
-};
+export type DACUser = DACContainer;
