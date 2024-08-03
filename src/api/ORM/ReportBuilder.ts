9b5ff048c0fabf7483ee7c9fc6f8287faadea91e
Ryanne Graff <ryantgraff@resistdesign.com>
2024-08-03 11:28:27 -0400

    chore: (repo) Automatic commit.

--- src/api/ORM/ReportBuilder.ts
+++ src/api/ORM/ReportBuilder.ts
@@ -1,1 +1,12 @@
 // TODO: Implement.
+
+// TODO: Approach:
+//  - Use the `TypeInfoORMService` to build a report.
+//  - Apply a queueing strategy to handle the report building.
+//    - Use queuing data types to organize queueing tasks.
+//    - Take queueing task related functions as input.
+//    - Track queueing carefully and consider things like:
+//      - Race conditions with multiple queue processors.
+//      - Not losing queued tasks.
+//      - Handling queueing errors.
+//      - Handling queueing timeouts and how much to process in a single batch.
