d5aca332cf982399e3872f46126771506867fa7f
Ryanne Graff <ryantgraff@resistdesign.com>
2024-08-10 00:20:50 -0400

    chore: (repo) Automatic commit.

--- src/app/components/TypeInfoComponents/Inputs/ObjectSearch.tsx
+++ src/app/components/TypeInfoComponents/Inputs/ObjectSearch.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
-import {LogicalOperators, SearchCriteria} from "../../../../common/SearchTypes";
+import {
+  LogicalOperators,
+  SearchCriteria,
+} from "../../../../common/SearchTypes";
 import { TypeInfoDataItem } from "../Types";
 import { TypeInfo } from "../../../../common/TypeParsing/TypeInfo";
 import { ObjectTable } from "./ObjectTable";
 import styled from "styled-components";
-import {Form} from "../../Form";
 
 // TODO: Search criteria interface.
 const BaseObjectSearch = styled.div`
@@ -16,19 +18,19 @@ const BaseObjectSearch = styled.div`
   gap: 1em;
 `;
 const Controls = styled.div`
-    flex: 1 0 auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: stretch;
-    gap: 1em;
+  flex: 1 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  gap: 1em;
 `;
 const FieldControl = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 1em;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 1em;
 `;
 
 export type ObjectSearchProps = {
@@ -42,36 +44,16 @@ export const ObjectSearch: FC<ObjectSearchProps> = ({
   searchCriteria,
   searchResults = [],
 }) => {
-  const {
-    logicalOperator = LogicalOperators.AND,
-    fieldCriteria = [],
-  } = searchCriteria;
-  const {
-    fields = {},
-  } = typeInfo;
+  const { logicalOperator = LogicalOperators.AND, fieldCriteria = [] } =
+    searchCriteria;
+  const { fields = {} } = typeInfo;
 
-  return <BaseObjectSearch>
-    <Controls>
-      {fieldCriteria.map((criteria, index) => {
-        const {} = criteria;
-        const Component
+  // TODO: Add and remove search fields.
 
-        return return InputComponent ? (
-          <label key={fieldName}>
-            <LabelText>{label}&nbsp;</LabelText>
-            <InputComponent
-              nameOrIndex={fieldName}
-              typeInfoField={field}
-              value={fieldValue}
-              onChange={onFieldChange}
-              options={tags}
-              onNavigateToType={onNavigateToType}
-            />
-            <LabelText>&nbsp;{label}</LabelText>
-          </label>
-        ) : undefined;
-      })}
-    </Controls>
-    <ObjectTable typeInfo={typeInfo} objectList={searchResults} />
-  </BaseObjectSearch>;
+  return (
+    <BaseObjectSearch>
+      <Controls></Controls>
+      <ObjectTable typeInfo={typeInfo} objectList={searchResults} />
+    </BaseObjectSearch>
+  );
 };
