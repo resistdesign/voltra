98270b787e35e771cbf641ac7bc6e956078e8408
Ryanne Graff <ryantgraff@resistdesign.com>
2024-08-11 17:09:57 -0400

    chore: (repo) Automatic commit.

--- src/app/components/TypeInfoComponents/Inputs/ObjectSearch.tsx
+++ src/app/components/TypeInfoComponents/Inputs/ObjectSearch.tsx
@@ -1,12 +1,18 @@
-import { FC } from "react";
+import { FC, useCallback } from "react";
 import {
   LogicalOperators,
   SearchCriteria,
 } from "../../../../common/SearchTypes";
-import { TypeInfoDataItem } from "../Types";
+import {
+  InputComponent,
+  NameOrIndex,
+  TypeInfoDataItem,
+  TypeNavigation,
+} from "../Types";
 import { TypeInfo } from "../../../../common/TypeParsing/TypeInfo";
 import { ObjectTable } from "./ObjectTable";
 import styled from "styled-components";
+import { TypeInfoInput } from "../TypeInfoInput";
 
 // TODO: Search criteria interface.
 const BaseObjectSearch = styled.div`
@@ -37,22 +43,52 @@ export type ObjectSearchProps = {
   typeInfo: TypeInfo;
   searchCriteria: SearchCriteria;
   searchResults: TypeInfoDataItem[];
+  onNavigateToType?: (typeNavigation: TypeNavigation) => void;
+  customInputTypeMap?: Record<string, InputComponent<any>>;
 };
 
 export const ObjectSearch: FC<ObjectSearchProps> = ({
   typeInfo,
   searchCriteria,
   searchResults = [],
+  onNavigateToType,
+  customInputTypeMap,
 }) => {
   const { logicalOperator = LogicalOperators.AND, fieldCriteria = [] } =
     searchCriteria;
   const { fields = {} } = typeInfo;
+  const onFieldChange = useCallback((nameOrIndex: NameOrIndex, value: any) => {
+    // TODO: Implement field change.
+  }, []);
 
   // TODO: Add and remove search fields.
 
   return (
     <BaseObjectSearch>
-      <Controls></Controls>
+      <Controls>
+        {fieldCriteria.map((fieldCriterionItem, index) => {
+          const { fieldName, value } = fieldCriterionItem;
+          const typeInfoField = fields[fieldName];
+
+          // TODO: Field selection. DO NOT ALLOW TypeReference fields.
+          // TODO: Operator selection.
+          // TODO: Add/remove criterion.
+
+          return (
+            <FieldControl key={`TypeInfoInput:${fieldName}:${index}`}>
+              <TypeInfoInput
+                typeInfoField={typeInfoField}
+                fieldValue={value}
+                nameOrIndex={fieldName}
+                onChange={onFieldChange}
+                onNavigateToType={onNavigateToType}
+                customInputTypeMap={customInputTypeMap}
+                ignoreTypeReferences
+              />
+            </FieldControl>
+          );
+        })}
+      </Controls>
       <ObjectTable typeInfo={typeInfo} objectList={searchResults} />
     </BaseObjectSearch>
   );
