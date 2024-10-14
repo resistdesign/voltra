3549772e0503721b594510f9d8e45acf1ec8af6a
Ryanne Graff <ryantgraff@resistdesign.com>
2024-10-14 19:26:46 -0400

    chore: (repo) Automatic commit.

---
+++ src/app/utils/Drivers/FetchDBServiceItemDriver.ts
@@ -0,0 +1,37 @@
+import { DBServiceItemDriver } from "../../../common/ServiceTypes";
+
+export class FetchDBServiceItemDriver<
+  ItemType extends Record<any, any>,
+  UniquelyIdentifyingFieldName extends keyof ItemType,
+> implements DBServiceItemDriver<ItemType, UniquelyIdentifyingFieldName>
+{
+  createItem(
+    newItem: Partial<Omit<ItemType, UniquelyIdentifyingFieldName>>,
+  ): Promise<ItemType[UniquelyIdentifyingFieldName]> {
+    throw new Error("Method not implemented.");
+  }
+
+  readItem(
+    uniqueIdentifier: ItemType[UniquelyIdentifyingFieldName],
+  ): Promise<ItemType> {
+    throw new Error("Method not implemented.");
+  }
+
+  updateItem(updatedItem: Partial<ItemType>): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+
+  deleteItem(
+    uniqueIdentifier: ItemType[UniquelyIdentifyingFieldName],
+  ): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+
+  listItems(
+    config: import("../../../common/SearchTypes").ListItemsConfig,
+  ): Promise<
+    boolean | import("../../../common/SearchTypes").ListItemsResults<ItemType>
+  > {
+    throw new Error("Method not implemented.");
+  }
+}
