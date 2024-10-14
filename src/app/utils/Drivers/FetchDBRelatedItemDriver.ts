9c8946254af7904d446cf13d3ecc56bbb6cd2c9e
Ryanne Graff <ryantgraff@resistdesign.com>
2024-10-14 19:30:16 -0400

    chore: (repo) Automatic commit.

--- src/app/utils/Drivers/FetchDBRelatedItemDriver.ts
+++ src/app/utils/Drivers/FetchDBRelatedItemDriver.ts
@@ -1,33 +1,15 @@
 import { DBRelatedItemDriver } from "../../../common/ServiceTypes";
 import { ItemRelationshipInfo } from "../../../common";
+import {
+  FetchDBServiceItemDriver,
+  FetchDBServiceItemDriverConfig,
+} from "./FetchDBServiceItemDriver";
 
-export class FetchDBRelatedItemDriver implements DBRelatedItemDriver {
-  createItem(
-    newItem: Partial<Omit<ItemRelationshipInfo, "id">>,
-  ): Promise<ItemRelationshipInfo["id"]> {
-    throw new Error("Method not implemented.");
-  }
-
-  readItem(
-    uniqueIdentifier: ItemRelationshipInfo["id"],
-  ): Promise<ItemRelationshipInfo> {
-    throw new Error("Method not implemented.");
-  }
-
-  updateItem(updatedItem: Partial<ItemRelationshipInfo>): Promise<boolean> {
-    throw new Error("Method not implemented.");
-  }
-
-  deleteItem(uniqueIdentifier: ItemRelationshipInfo["id"]): Promise<boolean> {
-    throw new Error("Method not implemented.");
-  }
-
-  listItems(
-    config: import("../../../common/SearchTypes").ListItemsConfig,
-  ): Promise<
-    | boolean
-    | import("../../../common/SearchTypes").ListItemsResults<ItemRelationshipInfo>
-  > {
-    throw new Error("Method not implemented.");
+export class FetchDBRelatedItemDriver
+  extends FetchDBServiceItemDriver<ItemRelationshipInfo, "id">
+  implements DBRelatedItemDriver
+{
+  constructor(protected config: FetchDBServiceItemDriverConfig) {
+    super(config);
   }
 }
