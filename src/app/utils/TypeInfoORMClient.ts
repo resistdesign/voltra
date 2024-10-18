a613b1a1c2ca5a0a056992a7123ffbbd7c487261
Ryanne Graff <ryantgraff@resistdesign.com>
2024-10-18 17:51:49 -0400

    chore: (repo) Automatic commit.

--- src/app/utils/TypeInfoORMClient.ts
+++ src/app/utils/TypeInfoORMClient.ts
@@ -1,5 +1,17 @@
-import { TypeInfoORMAPI } from "../../common/TypeInfoORM";
+import {
+  TypeInfoORMAPI,
+  TypeInfoORMAPIRoutePaths,
+} from "../../common/TypeInfoORM";
 import { sendServiceRequest, ServiceConfig } from "./Service";
+import { TypeInfoDataItem } from "../../common/TypeParsing/TypeInfo";
+import {
+  BaseItemRelationshipInfo,
+  ItemRelationshipInfo,
+  ItemRelationshipOriginatingItemInfo,
+  ListItemsConfig,
+  ListItemsResults,
+  ListRelationshipsConfig,
+} from "../../common";
 
 /**
  * A client for a TypeInfoORM API or service.
@@ -8,50 +20,94 @@ export class TypeInfoORMClient implements TypeInfoORMAPI {
   // TODO: Implement all methods.
   constructor(private config: ServiceConfig) {}
 
-  protected makeRequest = async (path: string, args: any[]): Promise<any> => {
+  protected makeRequest = async (
+    path: TypeInfoORMAPIRoutePaths,
+    args: any[],
+  ): Promise<any> => {
     const result = await sendServiceRequest(this.config, path, args);
 
     return result;
   };
 
-  create = async (typeName: string, item: any): Promise<any> => {
-    return null;
+  create = async (typeName: string, item: TypeInfoDataItem): Promise<any> => {
+    return await this.makeRequest(TypeInfoORMAPIRoutePaths.CREATE, [
+      typeName,
+      item,
+    ]);
   };
 
-  read = async (typeName: string, primaryFieldValue: any): Promise<any> => {
-    return null;
+  read = async (
+    typeName: string,
+    primaryFieldValue: any,
+  ): Promise<TypeInfoDataItem> => {
+    return await this.makeRequest(TypeInfoORMAPIRoutePaths.READ, [
+      typeName,
+      primaryFieldValue,
+    ]);
   };
 
-  update = async (typeName: string, item: any): Promise<boolean> => {
-    return false;
+  update = async (
+    typeName: string,
+    item: TypeInfoDataItem,
+  ): Promise<boolean> => {
+    return await this.makeRequest(TypeInfoORMAPIRoutePaths.UPDATE, [
+      typeName,
+      item,
+    ]);
   };
 
   delete = async (
     typeName: string,
     primaryFieldValue: any,
   ): Promise<boolean> => {
-    return false;
+    return await this.makeRequest(TypeInfoORMAPIRoutePaths.DELETE, [
+      typeName,
+      primaryFieldValue,
+    ]);
   };
 
-  list = async (typeName: string, config: any): Promise<boolean | any> => {
-    return null;
+  list = async (
+    typeName: string,
+    config: ListItemsConfig,
+  ): Promise<boolean | ListItemsResults<TypeInfoDataItem>> => {
+    return await this.makeRequest(TypeInfoORMAPIRoutePaths.LIST, [
+      typeName,
+      config,
+    ]);
   };
 
-  createRelationship = async (relationshipItem: any): Promise<string> => {
-    return "";
+  createRelationship = async (
+    relationshipItem: BaseItemRelationshipInfo,
+  ): Promise<string> => {
+    return await this.makeRequest(
+      TypeInfoORMAPIRoutePaths.CREATE_RELATIONSHIP,
+      [relationshipItem],
+    );
   };
 
-  deletedRelationship = async (relationshipItem: any): Promise<boolean> => {
-    return false;
+  deletedRelationship = async (
+    relationshipItem: BaseItemRelationshipInfo,
+  ): Promise<boolean> => {
+    return await this.makeRequest(
+      TypeInfoORMAPIRoutePaths.DELETE_RELATIONSHIP,
+      [relationshipItem],
+    );
   };
 
-  listRelationships = async (config: any): Promise<boolean | any> => {
-    return null;
+  listRelationships = async (
+    config: ListRelationshipsConfig,
+  ): Promise<boolean | ListItemsResults<ItemRelationshipInfo>> => {
+    return await this.makeRequest(TypeInfoORMAPIRoutePaths.LIST_RELATIONSHIPS, [
+      config,
+    ]);
   };
 
   cleanupRelationships = async (
-    relationshipOriginatingItem: any,
+    relationshipOriginatingItem: ItemRelationshipOriginatingItemInfo,
   ): Promise<void> => {
-    return null;
+    return await this.makeRequest(
+      TypeInfoORMAPIRoutePaths.CLEANUP_RELATIONSHIPS,
+      [relationshipOriginatingItem],
+    );
   };
 }
