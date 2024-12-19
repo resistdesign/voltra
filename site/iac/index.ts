d0ffa43c57303aa56448ed9594ce39b37af867a8
Ryanne Graff <ryantgraff@resistdesign.com>
2024-12-19 14:50:08 -0500

    feat: (site) Add the basis for IaC.

---
+++ site/iac/index.ts
@@ -0,0 +1,60 @@
+// We need an API to be able to test backend code.
+import { SimpleCFT } from "../../src/iac";
+import { addDNS, addSecureFileStorage } from "../../src/iac/packs";
+
+const IDS = {
+  PARAMETERS: {
+    HOSTED_ZONE_ID: "HostedZoneId",
+  },
+  API: {
+    FILE_STORAGE: "ApiFileStorage",
+    GATEWAY: "APIGateway",
+    FUNCTION: "APIFunction",
+  },
+};
+const BASE_DOMAIN = "demo.voltra.app";
+const DOMAINS = {
+  APP: `app.${BASE_DOMAIN}`,
+  API: `api.${BASE_DOMAIN}`,
+};
+
+const IaC = new SimpleCFT({
+  AWSTemplateFormatVersion: "2010-09-09",
+  Description: "The Voltra API Demo Backend",
+})
+  .addParameterGroup({
+    Label: "DNS",
+    Parameters: {
+      [IDS.PARAMETERS.HOSTED_ZONE_ID]: {
+        Label: "Hosted Zone ID",
+        Type: "String",
+        Description: "The Hosted Zone ID for the domain",
+      },
+    },
+  })
+  .applyPack(addDNS, {
+    hostedZoneIdParameterName: IDS.PARAMETERS.HOSTED_ZONE_ID,
+    domainNameParameterName: BASE_DOMAIN,
+  })
+  .applyPack(addSecureFileStorage, {
+    id: IDS.API.FILE_STORAGE,
+    bucketName: {
+      "Fn::Sub": [
+        "api-files.${BaseDomainName}",
+        {
+          BaseDomainName: { Ref: BASE_DOMAIN },
+        },
+      ],
+    },
+    shouldDelete: true,
+    blockPublicAccess: true,
+    cors: {
+      CorsRules: [
+        {
+          AllowedHeaders: ["*"],
+          AllowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+          AllowedOrigins: ["*"],
+        },
+      ],
+    },
+  });
