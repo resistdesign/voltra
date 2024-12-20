de1a3e6327d5af5fd3d2e31d51c8922d5d9a404e
Ryanne Graff <ryantgraff@resistdesign.com>
2024-12-20 12:23:46 -0500

    chore: (repo) Automatic commit.

--- site/app/index.tsx
+++ site/app/index.tsx
@@ -1,12 +1,13 @@
-import "@picocss/pico";
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
-
-const appRootElement = document.getElementById("app-root");
-
-if (appRootElement) {
-  const root = createRoot(appRootElement);
-
-  root.render(<App />);
-}
+// import "@picocss/pico";
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import { App } from "./App";
+//
+// const appRootElement = document.getElementById("app-root");
+//
+// if (appRootElement) {
+//   const root = createRoot(appRootElement);
+//
+//   root.render(<App />);
+// }
+console.log('ImAnApp');
