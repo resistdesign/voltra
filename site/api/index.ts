625799572480d7cd73c89fbc74fb2914b780b93d
Ryanne Graff <ryantgraff@resistdesign.com>
2024-12-20 17:07:10 -0500

    chore: (repo) Automatic commit.

--- site/api/index.ts
+++ site/api/index.ts
@@ -1,1 +1,24 @@
-export const ImAThing = {};
+import {
+  addRoutesToRouteMap,
+  AWS,
+  handleCloudFunctionEvent,
+} from "../../src/api/Router";
+import { CloudFunctionResponse, RouteMap } from "../../src/api/Router/Types";
+import normalizeCloudFunctionEvent = AWS.normalizeCloudFunctionEvent;
+
+const ROUTE_MAP: RouteMap = addRoutesToRouteMap({}, [
+  {
+    path: "/hello",
+    handler: async () => {
+      return "Hello, world!";
+    },
+    authConfig: {
+      public: true,
+    },
+  },
+]);
+
+export const handler = async (event: any): Promise<CloudFunctionResponse> =>
+  handleCloudFunctionEvent(event, normalizeCloudFunctionEvent, ROUTE_MAP, [
+    process.env.CLIENT_ORIGIN as string,
+  ]);
