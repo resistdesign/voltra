41a1a9a3d3377cd59ea4ccddce52fb0541093e56
Ryanne Graff <ryantgraff@resistdesign.com>
2024-12-27 15:58:21 -0500

    feat: (DynamoDBDataItemDBDriver) Complete the listItems method.

--- src/common/TypeParsing/Utils.ts
+++ src/common/TypeParsing/Utils.ts
@@ -4,32 +4,36 @@ import { TypeInfo, TypeInfoDataItem } from "./TypeInfo";
  * Remove all fields, from a list of selected fields, that are a type reference.
  * */
 export const removeTypeReferenceFieldsFromSelectedFields = <ItemType>(
-  selectFields: (keyof ItemType)[],
   typeInfo: TypeInfo = {},
-): (keyof ItemType)[] => {
-  const { fields = {} } = typeInfo;
-  const cleanSelectFields: (keyof ItemType)[] = [];
+  selectedFields?: (keyof ItemType)[],
+): (keyof ItemType)[] | undefined => {
+  if (Array.isArray(selectedFields)) {
+    const { fields = {} } = typeInfo;
+    const cleanSelectFields: (keyof ItemType)[] = [];
 
-  for (const tIF in fields) {
-    const { typeReference } = fields[tIF];
+    for (const tIF in fields) {
+      const { typeReference } = fields[tIF];
 
-    if (
-      typeof typeReference === "undefined" &&
-      selectFields.includes(tIF as keyof ItemType)
-    ) {
-      cleanSelectFields.push(tIF as keyof ItemType);
+      if (
+        typeof typeReference === "undefined" &&
+        selectedFields.includes(tIF as keyof ItemType)
+      ) {
+        cleanSelectFields.push(tIF as keyof ItemType);
+      }
     }
-  }
 
-  return cleanSelectFields;
+    return cleanSelectFields;
+  } else {
+    return selectedFields;
+  }
 };
 
 /**
  * Remove all fields, from a data item, that are a type reference.
  * */
 export const removeTypeReferenceFieldsFromDataItem = (
-  dataItem: TypeInfoDataItem = {},
   typeInfo: TypeInfo = {},
+  dataItem: TypeInfoDataItem = {},
 ): TypeInfoDataItem => {
   const { fields = {} } = typeInfo;
   const cleanItem: TypeInfoDataItem = {};
@@ -41,6 +45,25 @@ export const removeTypeReferenceFieldsFromDataItem = (
       typeof typeReference === "undefined" &&
       typeof dataItem[tIF] !== "undefined"
     ) {
+      cleanItem[tIF] = dataItem[tIF];
+    }
+  }
+
+  return cleanItem;
+};
+
+/**
+ * Remove all fields, from a data item, that are not in the type info.
+ * */
+export const removeNonexistentFieldsFromDataItem = (
+  typeInfo: TypeInfo = {},
+  dataItem: TypeInfoDataItem = {},
+): TypeInfoDataItem => {
+  const { fields = {} } = typeInfo;
+  const cleanItem: TypeInfoDataItem = {};
+
+  for (const tIF in dataItem) {
+    if (fields[tIF]) {
       cleanItem[tIF] = dataItem[tIF];
     }
   }
