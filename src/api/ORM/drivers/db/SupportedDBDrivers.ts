e690bf0f8ee565920182eb3dc12de29135dcf0ca
Ryanne Graff <ryantgraff@resistdesign.com>
2024-12-28 17:20:42 -0500

    chore: (repo) Automatic commit.

--- src/api/ORM/drivers/db/SupportedDBDrivers.ts
+++ src/api/ORM/drivers/db/SupportedDBDrivers.ts
@@ -1,13 +1,22 @@
 import { SupportedDataItemDBDriverEntry } from "../Types";
 import { DynamoDBSupportedDataItemDBDriverEntry } from "./DynamoDBDataItemDBDriver";
+import { S3SupportedDataItemDBDriverEntry } from "./S3DataItemDBDriver";
 
+/**
+ * The supported DB driver names.
+ * */
 export enum SupportedDBDriverNames {
   DYNAMO_DB = "DYNAMO_DB",
+  S3 = "S3",
 }
 
+/**
+ * A map of supported DB drivers by name.
+ * */
 export const SUPPORTED_DB_DRIVERS: Record<
   SupportedDBDriverNames,
   SupportedDataItemDBDriverEntry
 > = {
   [SupportedDBDriverNames.DYNAMO_DB]: DynamoDBSupportedDataItemDBDriverEntry,
+  [SupportedDBDriverNames.S3]: S3SupportedDataItemDBDriverEntry,
 };
--- src/api/ORM/drivers/db/index.ts
+++ src/api/ORM/drivers/db/index.ts
@@ -2,4 +2,4 @@ export * from "./S3DataItemDBDriver";
 export * from "./S3BasicDBDriver";
 export * as Utils from "./Utils";
 export * from "./DynamoDBDataItemDBDriver";
-export * from "./SupportedDBDriverNames";
+export * from "./SupportedDBDrivers";
