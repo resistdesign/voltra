a401e0f17f352fb533d8490fde0f089e56b4f190
Ryanne Graff <ryantgraff@resistdesign.com>
2025-01-20 00:48:20 -0500

    chore: (repo) Automatic commit.

--- src/common/SearchValidation.ts
+++ src/common/SearchValidation.ts
@@ -9,6 +9,7 @@ export const SEARCH_VALIDATION_ERRORS = {
   INVALID_TYPE_INFO: "INVALID_TYPE_INFO",
   RELATIONAL_FIELDS_NOT_ALLOWED: "RELATIONAL_FIELDS_NOT_ALLOWED",
   INVALID_FIELD: "INVALID_FIELD",
+  INVALID_VALUE_OPTION: "INVALID_VALUE_OPTION",
   INVALID_OPERATOR: "INVALID_OPERATOR",
 };
 
@@ -32,7 +33,7 @@ export const validateSearchFields = (
     const { fields = {} } = typeInfo;
 
     for (const f of searchFields) {
-      const { fieldName, operator, customOperator } = f;
+      const { fieldName, operator, customOperator, value, valueOptions } = f;
 
       if (
         !customOperator &&
@@ -46,9 +47,19 @@ export const validateSearchFields = (
         const tIF = fields[fieldName];
 
         if (tIF) {
-          const { typeReference, tags = {} } = tIF;
+          const { typeReference, tags = {}, possibleValues } = tIF;
 
-          if (disallowRelationalFields && typeReference) {
+          if (
+            Array.isArray(possibleValues) &&
+            ((Array.isArray(valueOptions) &&
+              !valueOptions.every((vO) => possibleValues.includes(vO))) ||
+              !possibleValues.includes(value))
+          ) {
+            results.valid = false;
+            results.errorMap[fieldName] = [
+              SEARCH_VALIDATION_ERRORS.INVALID_VALUE_OPTION,
+            ];
+          } else if (disallowRelationalFields && typeReference) {
             results.valid = false;
             results.errorMap[fieldName] = [
               SEARCH_VALIDATION_ERRORS.RELATIONAL_FIELDS_NOT_ALLOWED,
