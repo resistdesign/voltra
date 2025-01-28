44b9dab1f9decd76bb6ed8e87d2c1f46187406ab
Ryanne Graff <ryantgraff@resistdesign.com>
2025-01-28 12:54:51 -0500

    chore: (repo) Automatic commit.

---
+++ src/api/ORM/DACUtils.conditions.spec.ts
@@ -0,0 +1,48 @@
+import { getDACRoleHasAccessToDataItem } from "./DACUtils";
+import { OperationGroup } from "../../common/TypeInfoORM";
+import { DACConstraintType } from "../DataAccessControl";
+
+export const getDACRoleHasAccessToDataItemConditions: Parameters<
+  typeof getDACRoleHasAccessToDataItem
+> = [
+  ["DEMO", "APP"],
+  OperationGroup.ALL_OPERATIONS,
+  "Contact",
+  {
+    id: "a38bc40e-052d-47e2-89c5-6a1e5593bc7f",
+  },
+  {
+    primaryField: "id",
+    fields: {
+      id: {
+        type: "string",
+        array: false,
+        readonly: false,
+        optional: false,
+      },
+    },
+  },
+  {
+    id: "5fbf571d-ea6b-4008-b40f-706dfd430236",
+    childRoleIds: ["ec14ac8f-6fda-458c-b2df-e431dced0947"],
+    constraints: [],
+  },
+  (roleId: string) => ({
+    id: roleId,
+    childRoleIds: [],
+    constraints: [
+      {
+        type: DACConstraintType.ALLOW,
+        pathIsPrefix: true,
+        resourcePath: [
+          "DEMO",
+          "APP",
+          OperationGroup.ALL_OPERATIONS,
+          "Contact",
+          "a38bc40e-052d-47e2-89c5-6a1e5593bc7f",
+        ],
+      },
+    ],
+  }),
+  [],
+];
