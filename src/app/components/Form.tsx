6f4dfb40ade6671843e22039d19d71627f5cfbc9
Ryanne Graff <ryantgraff@resistdesign.com>
2025-01-30 00:39:24 -0500

    chore: (repo) Automatic commit.

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,27 +1,27 @@
 import React, { FC, FormEvent, FormHTMLAttributes } from "react";
 import styled from "styled-components";
 
-/**
- * @ignore
- * */
-export type FormBaseProps = FormHTMLAttributes<HTMLFormElement>;
-
-const FormBase: FC<FormBaseProps> = ({ onSubmit, ...rest }) => {
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    onSubmit?.(event);
-  };
-
-  return <form onSubmit={handleSubmit} {...rest} />;
-};
-
-/**
- * A basic form element wrapper that handles form submission in an expected and normalized way.
- * */
-export const Form = styled(FormBase).attrs((p) => ({
+const FormBase = styled.form.attrs((p) => ({
   className: "form-normalized-outer-spacing",
 }))`
   &.form-normalized-outer-spacing {
     margin-bottom: 0;
   }
 `;
+
+/**
+ * The props for the form component.
+ * */
+export type FormProps = FormHTMLAttributes<HTMLFormElement>;
+
+/**
+ * A basic form element wrapper that handles form submission in an expected and normalized way.
+ * */
+export const Form: FC<FormProps> = ({ onSubmit, ...rest }) => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSubmit?.(event);
+  };
+
+  return <FormBase onSubmit={handleSubmit} {...rest} />;
+};
