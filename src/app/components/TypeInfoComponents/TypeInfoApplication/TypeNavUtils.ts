e0c2b6eb275ba6f9cd8c689d8d027cdd8a47ccd1
Ryanne Graff <ryantgraff@resistdesign.com>
2025-03-08 17:35:20 -0500

    chore: (repo) Automatic commit.

--- src/app/components/TypeInfoComponents/TypeInfoApplication/TypeNavUtils.ts
+++ src/app/components/TypeInfoComponents/TypeInfoApplication/TypeNavUtils.ts
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { TypeNavigation, TypeNavigationMode } from "../Types";
 import { NonUpdateOperationMode, UpdateOperationMode } from "./Types";
 import { TypeOperation } from "../../../../common/TypeParsing/TypeInfo";
@@ -28,4 +28,33 @@ export const useBaseTypeNavigation = <BaseOperationType extends TypeOperation>({
     }),
     [baseTypeInfoName, basePrimaryKeyValue, baseMode, baseOperation],
   );
+};
+
+export const useTypeNavHistory = ({
+  baseTypeNavigation,
+}: {
+  baseTypeNavigation: TypeNavigation;
+}) => {
+  const [navHistory, setNavHistory] = useState<TypeNavigation[]>([]);
+  const relationshipMode = navHistory.length > 0;
+  const currentTypeNavigation = useMemo<TypeNavigation>(
+    () => navHistory[navHistory.length - 1] || baseTypeNavigation,
+    [navHistory, baseTypeNavigation],
+  );
+  const {
+    fromTypeName: currentFromTypeName,
+    fromTypePrimaryFieldValue: currentFromTypePrimaryFieldValue,
+    fromTypeFieldName: currentFromTypeFieldName,
+    operation: currentOperation,
+  } = currentTypeNavigation;
+
+  return {
+    navHistory,
+    setNavHistory,
+    relationshipMode,
+    currentFromTypeName,
+    currentFromTypePrimaryFieldValue,
+    currentFromTypeFieldName,
+    currentOperation,
+  };
 };
