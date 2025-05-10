2c5b47eee6b4f76bb2bdd0a61b1bf82591f5828e
Ryanne Graff <ryantgraff@resistdesign.com>
2025-05-10 02:04:13 -0400

    chore: (repo) Automatic commit.

--- site/common/Constants.ts
+++ site/common/Constants.ts
@@ -1,9 +1,10 @@
 // @ts-ignore
-import DEMO_TS from "bundle-text:../../../common/Types";
+import DEMO_TS from "bundle-text:./Types";
 import { getTypeInfoMapFromTypeScript } from "../../src/common/TypeParsing";
 import { TypeInfoMap } from "../../src/common/TypeParsing/TypeInfo";
 
-export const DEMO_TYPE_INFO_MAP: TypeInfoMap =
-  getTypeInfoMapFromTypeScript(DEMO_TS);
+export const DEMO_TYPE_INFO_MAP: TypeInfoMap = getTypeInfoMapFromTypeScript(
+  DEMO_TS as string,
+);
 
 export const DEMO_ORM_ROUTE_PATH = "/db";
