208073879b0a8249560dba74ba74faca049c1692
Ryanne Graff <ryanne.graff@resistdesign.com>
2025-07-16 23:12:22 -0400

    chore: (repo) Automatic commit.

--- site/common/Constants.ts
+++ site/common/Constants.ts
@@ -1,11 +1,3 @@
-import { getTypeInfoMapFromTypeScript } from "../../src/common/TypeParsing";
-import { TypeInfoMap } from "../../src/common/TypeParsing/TypeInfo";
-// @ts-ignore
-import DEMO_TS from "raw:./Types";
-
-export const DEMO_TYPE_INFO_MAP: TypeInfoMap =
-  getTypeInfoMapFromTypeScript(DEMO_TS);
-
 export const DEMO_ORM_ROUTE_PATH = "/db";
 export const BASE_DOMAIN = "demo.voltra.app";
 export const DOMAINS = {
