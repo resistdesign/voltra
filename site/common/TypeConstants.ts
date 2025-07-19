18596ec037176cd60e4ff1138d2b4604b3cbbb57
Ryanne Graff <ryanne.graff@resistdesign.com>
2025-07-18 23:43:00 -0400

    chore: (repo) Automatic commit.

--- site/common/TypeConstants.ts
+++ site/common/TypeConstants.ts
@@ -1,7 +1,6 @@
 import { getTypeInfoMapFromTypeScript } from "../../src/common/TypeParsing";
 import { TypeInfoMap } from "../../src/common/TypeParsing/TypeInfo";
-// @ts-ignore
-import DEMO_TS from "raw:./Types.ts";
+import DEMO_TS from "source:./Types";
 
 export const DEMO_TYPE_INFO_MAP: TypeInfoMap =
   getTypeInfoMapFromTypeScript(DEMO_TS);
