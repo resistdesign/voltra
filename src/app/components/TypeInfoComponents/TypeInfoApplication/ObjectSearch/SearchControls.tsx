e0235299d62b6dba4bba75a91ea8c170f46a0024
Ryanne Graff <ryanne.graff@resistdesign.com>
2025-07-29 21:03:51 -0400

    chore: (repo) Automatic commit.

--- src/app/components/TypeInfoComponents/TypeInfoApplication/ObjectSearch/SearchControls.tsx
+++ src/app/components/TypeInfoComponents/TypeInfoApplication/ObjectSearch/SearchControls.tsx
@@ -29,7 +29,7 @@ const BaseSearchControls = styled.div`
   gap: 1em;
 `;
 const FieldCriterionControlItem = styled.div`
-  flex: 1 0 auto;
+  flex: 0 0 auto;
   display: flex;
   flex-direction: row;
   justify-content: stretch;
