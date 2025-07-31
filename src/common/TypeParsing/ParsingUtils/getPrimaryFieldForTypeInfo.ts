4ad34a5f05cc013ee862df749e90366db991ed96
Ryanne Graff <ryanne.graff@resistdesign.com>
2025-07-31 00:50:21 -0400

    chore: (repo) Automatic commit.

--- src/common/TypeParsing/ParsingUtils/getPrimaryFieldForTypeInfo.ts
+++ src/common/TypeParsing/ParsingUtils/getPrimaryFieldForTypeInfo.ts
@@ -1,19 +1,40 @@
 import { TypeInfo } from "../TypeInfo";
 
+export enum TypeInfoPrimaryFieldErrors {
+  READ_DENIED_PRIMARY_FIELD_NOT_SUPPORTED = "READ_DENIED_PRIMARY_FIELD_NOT_SUPPORTED",
+}
+
 export const getPrimaryFieldForTypeInfo = (
   typeInfo: TypeInfo,
 ): string | undefined => {
   const { fields } = typeInfo;
 
-  let primaryField: string | undefined = undefined;
+  let primaryField: string | undefined = undefined,
+    primaryFieldReadDenied: boolean = false;
 
   for (const fieldName in fields) {
     const field = fields[fieldName];
-    const { tags: { primaryField: isPrimaryField = false } = {} } = field;
+    const {
+      tags: {
+        primaryField: isPrimaryField = false,
+        deniedOperations: { READ: readDenied = false } = {},
+      } = {},
+    } = field;
 
     if (isPrimaryField || !primaryField) {
       primaryField = fieldName;
+      primaryFieldReadDenied = readDenied;
     }
+
+    if (isPrimaryField) {
+      break;
+    }
+  }
+
+  if (primaryFieldReadDenied) {
+    throw new Error(
+      TypeInfoPrimaryFieldErrors.READ_DENIED_PRIMARY_FIELD_NOT_SUPPORTED,
+    );
   }
 
   return primaryField;
