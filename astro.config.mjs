f5334893989df35b4a74db314d259effc3c2f338
RyAnne Graff <ryanne@resist.design>
2025-12-21 22:23:13 -0500

    chore: (repo) Automatic commit.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  vite: {
+    plugins: [basicSsl()],
+  },
   integrations: [react()],
   srcDir: "./site/app/src",
   publicDir: "./public",
