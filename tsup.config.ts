dc1eaf0844876e33b328a2d8ef3405b65883e102
RyAnne Graff <ryanne@resist.design>
2025-12-30 17:26:22 -0500

    chore: (repo) Automatic commit.

---
+++ tsup.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: {
+    index: "src/index.ts",
+    "api/index": "src/api/index.ts",
+    "app/index": "src/app/index.ts",
+    "common/index": "src/common/index.ts",
+    "iac/index": "src/iac/index.ts",
+    "iac/packs/index": "src/iac/packs/index.ts",
+  },
+  format: ["esm"],
+  platform: "node",
+  target: "es2022",
+  bundle: true,
+  splitting: false,
+  treeshake: true,
+  dts: true,
+  clean: true,
+  outDir: "dist",
+  outExtension: () => ({ js: ".js" }),
+  tsconfig: "tsconfig.build.json",
+});
