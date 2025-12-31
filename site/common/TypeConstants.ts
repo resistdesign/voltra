199b8bf2fcb9d23562cb9713bcd14902877a9ebb
Ryanne Graff <ryanne@resist.design>
2025-12-31 02:12:56 -0500

    chore: (repo) Automatic commit. (#283)

--- site/common/TypeConstants.ts
+++
@@ -1,6 +0,0 @@
-import { getTypeInfoMapFromTypeScript } from "../../src/common/TypeParsing";
-import { TypeInfoMap } from "../../src/common/TypeParsing/TypeInfo";
-import DEMO_TS from "source:./Types";
-
-export const DEMO_TYPE_INFO_MAP: TypeInfoMap =
-  getTypeInfoMapFromTypeScript(DEMO_TS);
--- site/common/Types.ts
+++ site/common/Types.ts
@@ -1,4 +1,4 @@
-export type PersistableItem = {
+type PersistableItem = {
   /**
    * @label ID
    * @primaryField
