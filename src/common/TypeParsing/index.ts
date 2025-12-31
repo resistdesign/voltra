110538dff54fd9d1d19ab9e1067acae7f251149a
Ryanne Graff <ryanne@resist.design>
2025-12-31 03:39:53 -0500

    Chore/site api build prebuild demo types (#284)
    * chore: (repo) Automatic commit.
    
    * chore: (repo) Automatic commit.
    
    * chore: (repo) Automatic commit.
    
    * chore: (repo) Automatic commit.
    
    * chore: (repo) Automatic commit.

--- src/common/TypeParsing/index.ts
+++ src/common/TypeParsing/index.ts
@@ -1,10 +1,8 @@
 /**
  * @packageDocumentation
  *
- * Type parsing utilities for deriving TypeInfo from TypeScript source.
  * Includes TypeInfo definitions, validation helpers, and AST mapping.
  */
-export * from "./TypeParsing";
 export * as Validation from "./Validation";
 export * as TypeInfo from "./TypeInfo";
 export * as TypeMapping from "./TypeMapping";
