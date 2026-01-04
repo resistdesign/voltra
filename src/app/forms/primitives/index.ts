9ec8bcb825414de4289a864c12679b18085ef713
RyAnne Graff <ryanne@resist.design>
2026-01-04 02:13:34 -0500

    fix: (site-form-gen) Fix styles and routing setup.

--- src/app/forms/primitives/index.ts
+++ src/app/forms/primitives/index.ts
@@ -16,7 +16,6 @@ export const FieldWrapper = styled("div")`
 export const Label = styled("label")`
   font-weight: 500;
   font-size: 0.875rem;
-  color: #333;
 `;
 
 export const Input = styled("input")`
