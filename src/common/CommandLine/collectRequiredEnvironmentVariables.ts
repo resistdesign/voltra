2b56755ed59e26dd2247c99ec9615151cc67f00b
Ryanne Graff <ryanne@resist.design>
2026-01-08 13:14:06 -0500

    Refactor/api indexing tables deployment (#305)
    * chore: (repo) Automatic commit.
    
    * chore: (repo) Automatic commit.
    
    * chore: (repo) Automatic commit.
    
    * chore: (repo) Automatic commit.

--- src/common/CommandLine/collectRequiredEnvironmentVariables.ts
+++ src/common/CommandLine/collectRequiredEnvironmentVariables.ts
@@ -10,16 +10,32 @@ export const collectRequiredEnvironmentVariables = <VarName extends string>(
   varNames: VarName[],
 ): Record<VarName, string> => {
   const result: Record<VarName, string> = {} as any;
+  const missingEnvVars: VarName[] = [];
 
   for (const vN of varNames) {
     const value: string | undefined = process.env[vN];
 
     // IMPORTANT: Verify that we have all required environment variables.
     if (!value) {
-      throw new Error(`Missing required environment variable: ${vN}`);
+      missingEnvVars.push(vN);
+    } else {
+      result[vN] = value;
     }
+  }
 
-    result[vN] = value;
+  if (missingEnvVars.length > 0) {
+    throw new Error(
+      `
+┌──────────────────────────────────┐
+│   ⚠  ENVIRONMENT CHECK FAILED    │
+└──────────────────────────────────┘
+
+Missing Required Environment Variables:
+
+${missingEnvVars.map((vN) => `- ${vN}`).join(`
+`)}
+`,
+    );
   }
 
   return result;
