6aa412ffd2dc74930302f555f20be44fa105b6a2
RyAnne Graff <ryanne@resist.design>
2026-02-08 21:26:15 -0500

    chore: (repo) Automatic commit.

--- src/api/index.ts
+++ src/api/index.ts
@@ -40,3 +40,5 @@ export * as Indexing from "./Indexing";
 export * as ORM from "./ORM";
 export * as Routing from "./Router";
 export * as DAC from "./DataAccessControl";
+
+export * from "./Router";
