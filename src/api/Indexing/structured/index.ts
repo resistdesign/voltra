1532147a779181ebcda5ced169e1e34145aead51
Resist Design <ryanne@resist.design>
2026-09-20 04:20:00 -0400

    refactor: remove in-memory implementations from structured barrel

--- src/api/Indexing/structured/index.ts
+++ src/api/Indexing/structured/index.ts
@@ -1,7 +1,5 @@
 export * from "./Cursor";
 export * from "./Handlers";
-export * from "./StructuredInMemoryIndex";
-export * from "./StructuredInMemoryBackend";
 export * from "./StructuredIndexRecords";
 export * from "./StructuredWriter";
 export * from "./StructuredOccupancy";
