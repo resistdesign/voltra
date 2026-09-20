2cb55cee9f6c4b64aee35152bd08cfa2e3908202
Resist Design <ryanne@resist.design>
2026-09-20 13:25:43 -0400

    test: remove Dynamo key limit from generic index tests

--- src/api/Indexing/IndexTable.test-utils.ts
+++ src/api/Indexing/IndexTable.test-utils.ts
@@ -1,6 +1,5 @@
 import {
   INDEX_ITEM_KINDS,
-  assertIndexTableKey,
   buildIndexKey,
   buildIndexDocumentSortKey,
   decodeIndexDocumentSortKey,
@@ -93,15 +92,6 @@ export const runIndexTableStructuredCollisionScenario = () => {
 export const runIndexTableRejectsMalformedUnicodeScenario = () => {
   try {
     encodeIndexIdentity("\ud800");
-    return false;
-  } catch (_error) {
-    return true;
-  }
-};
-
-export const runIndexTableRejectsOversizedKeyScenario = () => {
-  try {
-    assertIndexTableKey({ pk: "p", sk: "x".repeat(1025) });
     return false;
   } catch (_error) {
     return true;
