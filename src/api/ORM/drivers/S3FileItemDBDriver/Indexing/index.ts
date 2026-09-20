40085f051938c8fd917cf570c7a7f467183adb90
Resist Design <ryanne@resist.design>
2026-09-20 15:29:58 -0400

    feat: export complete S3 indexing backends

--- src/api/ORM/drivers/S3FileItemDBDriver/Indexing/index.ts
+++ src/api/ORM/drivers/S3FileItemDBDriver/Indexing/index.ts
@@ -3,3 +3,8 @@
  */
 export * from "./ExactS3";
 export * from "./LossyS3";
+export * from "./S3IndexObjectStore";
+export * from "./S3IndexRecordStore";
+export * from "./FullTextS3Backend";
+export * from "./StructuredS3Backend";
+export * from "./RelationalS3Backend";
