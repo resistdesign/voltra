94f5ab004f4f266a9209e9735ebe46b92d152598
Resist Design <ryanne@resist.design>
2026-09-24 01:06:44 -0400

    Remove obsolete maintenance-index runtime validation

--- src/api/ORM/drivers/DynamoDBDataItemDBDriver/Indexing/IndexingTablesValidation.test-utils.ts
+++ src/api/ORM/drivers/DynamoDBDataItemDBDriver/Indexing/IndexingTablesValidation.test-utils.ts
@@ -1,7 +1,4 @@
-import {
-  assertDynamoIndexTableKey,
-  assertIndexTableConfig,
-} from "./IndexTable";
+import { assertDynamoIndexTableKey } from "./IndexTable";
 import type { DynamoQueryClient } from "./Types";
 import { FullTextDdbBackend } from "./FullTextDdbBackend";
 import { createRelationEdgesDdbDependencies } from "./RelationalDdb";
@@ -71,17 +68,5 @@ export const runDynamoIndexRejectsOversizedKeyScenario = () => {
     return false;
   } catch (_error) {
     return true;
-  }
-};
-
-export const runIndexingTablesValidationEmptyMaintenanceIndexScenario = () => {
-  try {
-    assertIndexTableConfig({
-      tableName: "Index",
-      maintenanceIndexName: "",
-    });
-    return undefined;
-  } catch (error) {
-    return (error as Error).message ?? String(error);
   }
 };
